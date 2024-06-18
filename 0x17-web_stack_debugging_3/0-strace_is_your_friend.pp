f8dc3fa854f4110a380cf92fdba14bc6820e9ebe
2005-annabel <125447128+2005-annabel@users.noreply.github.com>
2024-06-18 22:15:14 +0300

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# the automated puppet for Apache 500 error
-
-exec { 'Fix wordpress site':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
-}
--- 0x17-web_stack_debugging_3/README.md
+++
@@ -1,1 +0,0 @@
-using strance to identify error and automating fix with puppet
